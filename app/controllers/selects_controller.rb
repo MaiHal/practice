38306f3e537780ae55a944f3ae2b9c9f06dd2984
MaiHal <myturn.lovenyc@gmail.com>
2018-10-18 23:29:31 +0900

    API使用を試みた

--- app/controllers/selects_controller.rb
+++ app/controllers/selects_controller.rb
@@ -1,6 +1,45 @@
 class SelectsController < ApplicationController
     def index
         @menu = Menu.all
+        uri = URI.parse("https://app.rakuten.co.jp/services/api/Recipe/CategoryList/20170426?format=json&applicationId=#{ENV['APPLICATION_ID']}&categoryId=41")
+        # リクエストパラメタを、インスタンス変数に格納
+        @query = uri.query
+
+        # 新しくHTTPセッションを開始し、結果をresponseへ格納
+        response = Net::HTTP.start(uri.host, uri.port) do |http|
+        # 接続時に待つ最大秒数を設定
+        http.open_timeout = 5
+        # 読み込み一回でブロックして良い最大秒数を設定
+        http.read_timeout = 10
+        # ここでWebAPIを叩いている
+        # Net::HTTPResponseのインスタンスが返ってくる
+        http.get(uri.request_uri)
+        end
+        # 例外処理の開始
+        begin
+        # responseの値に応じて処理を分ける
+        case response
+        # 成功した場合
+        when Net::HTTPSuccess
+            # responseのbody要素をJSON形式で解釈し、hashに変換
+            @result = JSON.parse(response.body)
+        # 別のURLに飛ばされた場合
+        when Net::HTTPRedirection
+            @message = "Redirection: code=#{response.code} message=#{response.message}"
+        # その他エラー
+        else
+            @message = "HTTP ERROR: code=#{response.code} message=#{response.message}"
+        end
+        # エラー時処理
+        rescue IOError => e
+        @message = "e.message"
+        rescue TimeoutError => e
+        @message = "e.message"
+        rescue JSON::ParserError => e
+        @message = "e.message"
+        rescue => e
+        @message = "e.message"
+        end
     end
 
     def show
