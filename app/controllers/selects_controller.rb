1823f50cc7497033db7b4e14fbbd4b03aab916ba
MaiHal <myturn.lovenyc@gmail.com>
2018-10-19 17:51:17 +0900

    ブランチ変えるためにコミット

--- app/controllers/selects_controller.rb
+++ app/controllers/selects_controller.rb
@@ -2,20 +2,19 @@ class SelectsController < ApplicationController
     require 'net/http'
     require 'uri'
     require 'json'
+    require 'pp'
 
     def index
         @menu = Menu.all
-        uri = URI.parse('https://app.rakuten.co.jp/services/api/Recipe/CategoryList/20170426?format=json&categoryType=large&applicationId=1025646104690209174')
-        http = Net::HTTP.new(uri.host, uri.port)
-        http.use_ssl = true
-        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        uri = URI.parse("https://app.rakuten.co.jp/services/api/Recipe/CategoryList/20170426?format=json&applicationId=#{ENV['APPLICATION_ID']}")
+        conn = Faraday::Connection.new(:url => uri) do |builder|
+            builder.use Faraday::Request::UrlEncoded
+            builder.use Faraday::Response::Logger
+            builder.use Faraday::Adapter::NetHttp
+        end
 
-        req = Net::HTTP::Post.new(uri.path)
-        #response = Net::HTTP.get_response()
-        res = http.request(req)
-
-        #result = ActiveSupport::JSON.decode(res.body)
-        render json: res
+        res = Faraday.new(:url => "https://app.rakuten.co.jp/services/api/Recipe/CategoryList/20170426?format=json&applicationId=#{ENV['APPLICATION_ID']}").get("&categoryType=large")
+        pp res
     end
 
     def show
